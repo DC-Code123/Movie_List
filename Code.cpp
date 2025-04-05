ed82273005c0f698d2062ac51e9db949a7752ae9
DC-Code123 <Distinctcode123@gmail.com>
2025-04-05 13:37:51 +0300

    Update Code.cpp

--- Code.cpp
+++ Code.cpp
@@ -20,38 +20,38 @@ void Movie_O(const M_info &movie) {
 
 // Function to input movie details
 void Movie_I(M_info &movie) {
-    cout << "What is the title of the movie?: ";
-    cin >> movie.title;
-    cout << "What is the year of release?: ";
-    cin >> movie.year;
-    cout << "What is the price of the movie?: ";
-    cin >> movie.price;
+  cout << "What is the title of the movie?: ";
+  cin >> movie.title;
+  cout << "What is the year of release?: ";
+  cin >> movie.year;
+  cout << "What is the price of the movie?: ";
+  cin >> movie.price;
 }
 
 int main() {
-    cout << "Welcome to the Movie Database!" << endl;
+  cout << "Welcome to the Movie Database!" << endl;
 
-    vector<M_info> movies; // Dynamic list of movies
-    char choice;
+  vector<M_info> movies; // Dynamic list of movies
+  char choice;
 
-    // Loop to allow the user to input multiple movies
-    do {
-        M_info movie; // Temporary variable to store movie details
-        cout << "Enter details for a new movie:" << endl;
-        Movie_I(movie); // Input movie details
-        movies.push_back(movie); // Add the movie to the list
+  // Loop to allow the user to input multiple movies
+  do {
+    M_info movie; // Temporary variable to store movie details
+    cout << "Enter details for a new movie:" << endl;
+    Movie_I(movie); // Input movie details
+    movies.push_back(movie); // Add the movie to the list
 
-        cout << "Do you want to enter another movie? (y/n): ";
-        cin >> choice;
-    } while (choice == 'y' || choice == 'Y');
+    cout << "Do you want to enter another movie? (y/n): ";
+    cin >> choice;
+  } while (choice == 'y' || choice == 'Y');
 
-    // Output all movie details
-    cout << "\nMovie Details:" << endl;
-    for (size_t i = 0; i < movies.size(); i++) {
+  // Output all movie details
+  cout << "\nMovie Details:" << endl;
+  for (size_t i = 0; i < movies.size(); i++) {
         cout << "Movie " << i + 1 << ":" << endl;
         Movie_O(movies[i]); // Display movie details
         cout << endl;
-    }
+  }
 
     cout << "Thank you for using the Movie Database!" << endl;
     cout << "Have a nice day!" << endl;
