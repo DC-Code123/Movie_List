55646a10b09425405115ed259d3117a10931c3a7
DC-Code123 <Distinctcode123@gmail.com>
2025-04-05 15:26:33 +0300

    Update Code.cpp

--- Code.cpp
+++ Code.cpp
@@ -12,6 +12,7 @@ struct M_info {
 };
 
 // Function to display movie details
+// Takes a constant reference to an M_info object to avoid copying
 void Movie_O(const M_info &movie) {
     cout << "The title of the movie is: " << movie.title << endl;
     cout << "The year of release is: " << movie.year << endl;
@@ -19,40 +20,43 @@ void Movie_O(const M_info &movie) {
 }
 
 // Function to input movie details
+// Takes a reference to an M_info object to update its fields
 void Movie_I(M_info &movie) {
-  cout << "What is the title of the movie?: ";
-  cin >> movie.title;
-  cout << "What is the year of release?: ";
-  cin >> movie.year;
-  cout << "What is the price of the movie?: ";
-  cin >> movie.price;
+    cout << "What is the title of the movie?: ";
+    cin >> movie.title; // Input movie title
+    cout << "What is the year of release?: ";
+    cin >> movie.year; // Input year of release
+    cout << "What is the price of the movie?: ";
+    cin >> movie.price; // Input price of the movie
 }
 
 int main() {
-  cout << "Welcome to the Movie Database!" << endl;
+    cout << "Welcome to the Movie Database!" << endl;
 
-  vector<M_info> movies; // Dynamic list of movies
-  char choice;
+    vector<M_info> movies; // Dynamic list of movies
+    char choice; // Variable to store user's choice to continue or stop
 
-  // Loop to allow the user to input multiple movies
-  do {
-    M_info movie; // Temporary variable to store movie details
-    cout << "Enter details for a new movie:" << endl;
-    Movie_I(movie); // Input movie details
-    movies.push_back(movie); // Add the movie to the list
+    // Loop to allow the user to input multiple movies
+    do {
+        M_info movie; // Temporary variable to store movie details
+        cout << "Enter details for a new movie:" << endl;
+        Movie_I(movie); // Input movie details
+        movies.push_back(movie); // Add the movie to the list
 
-    cout << "Do you want to enter another movie? (y/n): ";
-    cin >> choice;
-  } while (choice == 'y' || choice == 'Y');
+        // Ask the user if they want to enter another movie
+        cout << "Do you want to enter another movie? (y/n): ";
+        cin >> choice;
+    } while (choice == 'y' || choice == 'Y'); // Continue if the user enters 'y' or 'Y'
 
-  // Output all movie details
-  cout << "\nMovie Details:" << endl;
-  for (size_t i = 0; i < movies.size(); i++) {
+    // Output all movie details
+    cout << "\nMovie Details:" << endl;
+    for (size_t i = 0; i < movies.size(); i++) {
         cout << "Movie " << i + 1 << ":" << endl;
         Movie_O(movies[i]); // Display movie details
         cout << endl;
-  }
+    }
 
+    // Thank the user for using the program
     cout << "Thank you for using the Movie Database!" << endl;
     cout << "Have a nice day!" << endl;
 
