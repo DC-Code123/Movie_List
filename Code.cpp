88dd774fcc8567c041345073298f9b255c70e691
DC-Code123 <Distinctcode123@gmail.com>
2025-04-05 12:25:10 +0300

    listing

--- Code.cpp
+++ Code.cpp
@@ -1,50 +1,61 @@
-#include<iostream>
-#include<vector>
-#include <algorithm>
+#include <iostream>
+#include <vector>
 #include <string>
 
 using namespace std;
-// Function for outputs
-void Movie_O(string title,int year,double price){
-  cout<<"The title of the movie is "<<title<<endl;
-  cout<<"The year of release is "<<year<<endl;
-  cout<<"The price of the movie nowadays is "<<price+1.0<<endl;
-  // This is to check if it worked
-  return"\n\nThis worked!!!";
+
+// Struct to store movie information
+struct M_info {
+    string title; // Movie title
+    int year;     // Year of release
+    double price; // Price of the movie
+};
+
+// Function to display movie details
+void Movie_O(const M_info &movie) {
+    cout << "The title of the movie is: " << movie.title << endl;
+    cout << "The year of release is: " << movie.year << endl;
+    cout << "The price of the movie nowadays is: $" << movie.price + 1.0 << endl;
 }
-// Function for inputs
-void Movie_I(string title,int year,double price){
-  cout<<"What is the title of the movie?: "<<endl;
-  cin>>title;
-  cout<<"What is the year of release?: "<<endl;
-  cin>>year;
-  cout<<"What is the price of the movie?: "<<endl;
-  cin>>price;
-  // After running this function, the values of title, year, and price will be updated
-  // in the main program.
-  return title,year,price;
+
+// Function to input movie details
+void Movie_I(M_info &movie) {
+    cout << "What is the title of the movie?: ";
+    cin >> movie.title;
+    cout << "What is the year of release?: ";
+    cin >> movie.year;
+    cout << "What is the price of the movie?: ";
+    cin >> movie.price;
 }
-// Main Program
-int main(){
-  string M_title;
-  int M_year;
-  double M_price;
-  cout<<"Welcome to the Movie Database!"<<endl;
-  cout<<"How many movies do you want to enter?: "<<endl;
-  int n;
-  cin>>n;
-  // Loop for the number of movies inputted
-  for(int i=n;i>0;i--){
-    Movie_I(M_title,M_year,M_price);
-    M_title = title;
-    M_year = year;
-    M_price = price;
-    cout<<endl;
-    Movie_O(M_title,M_year,M_price);
-    cout<<endl;
-  }
-  cout<<"Thank you for using the Movie Database!"<<endl;
-  cout<<"Have a nice day!"<<endl;
-  // Terminates the program
-  return 0;
+
+int main() {
+    cout << "Welcome to the Movie Database!" << endl;
+
+    vector<M_info> movies; // Dynamic list of movies
+    char choice;
+
+    // Loop to allow the user to input multiple movies
+    do {
+        M_info movie; // Temporary variable to store movie details
+        cout << "Enter details for a new movie:" << endl;
+        Movie_I(movie); // Input movie details
+        movies.push_back(movie); // Add the movie to the list
+
+        cout << "Do you want to enter another movie? (y/n): ";
+        cin >> choice;
+    } while (choice == 'y' || choice == 'Y');
+
+    // Output all movie details
+    cout << "\nMovie Details:" << endl;
+    for (size_t i = 0; i < movies.size(); i++) {
+        cout << "Movie " << i + 1 << ":" << endl;
+        Movie_O(movies[i]); // Display movie details
+        cout << endl;
+    }
+
+    cout << "Thank you for using the Movie Database!" << endl;
+    cout << "Have a nice day!" << endl;
+
+    // Terminates the program
+    return 0;
 }
